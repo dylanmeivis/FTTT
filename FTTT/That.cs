2e363e1a2ae5ff423de1bd3f65b8389461c4db81
Dylan Meivis <dmeivis@accepteasy.com>
2020-10-06 19:57:08 +0200

    Create That class

---
+++ FTTT/That.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FTTT
+{
+    public class That<T>
+    {
+        internal T Result { get; set; }
+
+        public That<TD> Then<TD>(Func<TD> v, out TD result)
+        {
+            result = v();
+            return new That<TD>
+            {
+                Result = v()
+            };
+        }
+        public That<TD> Then<TD>(TD v, out TD result)
+        {
+            result = v;
+            return new That<TD>
+            {
+                Result = v
+            };
+        }
+
+        public That<TD> Then<TD>(Func<TD> v)
+        {
+            return new That<TD>
+            {
+                Result = v()
+            };
+        }
+        public That<TD> Then<TD>(TD v)
+        {
+            return new That<TD>
+            {
+                Result = v
+            };
+        }
+
+        public T Resolve()
+        {
+            return Result;
+        }
+    }
+}
