691203df783b449193cc192e5854d4f3bbf72897
Dylan Meivis <dmeivis@accepteasy.com>
2020-10-06 19:57:01 +0200

    Create First class

---
+++ FTTT/First.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FTTT
+{
+    public static class First
+    {
+        public static That<T> This<T>(Func<T> v, out T result)
+        {
+            result = v();
+            return new That<T>
+            {
+                Result = v()
+            };
+        }
+
+        public static That<T> This<T>(T v, out T result)
+        {
+            result = v;
+            return new That<T>
+            {
+                Result = v
+            };
+        }
+
+        public static That<T> This<T>(Func<T> v)
+        {
+            return new That<T>
+            {
+                Result = v()
+            };
+        }
+        public static That<T> This<T>(T v)
+        {
+            return new That<T>
+            {
+                Result = v
+            };
+        }
+    }
+}
